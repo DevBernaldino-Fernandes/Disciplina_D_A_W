f9239465bacaaae5736ebf469a4ed8dde1b0c00e
DevBernaldino-fernandes <bernaldinofernandes@hotmail.com>
2025-01-22 23:31:13 -0100

    primeiro commit

---
+++ 6. site_piaget/js/script.js
@@ -0,0 +1,7 @@
+// const piaget = document.querySelector('.piaget');
+// const internacional = document.querySelector('.internacional');
+// const dropdown = document.querySelector('.menu-dropdown')
+
+// perfil.onclick = ()=>{
+//     menubar.classList.add('.ativo')
+// }
---
+++ README.md
@@ -0,0 +1,1 @@
+# Disciplina_D_A_W
